e7176be744e3fa68b8dde99d7d17841559a6c870
david-o-rosario1201 <finnnn023@gmail.com>
2024-02-18 12:38:04 -0400

    Contexto listo

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,6 +1,16 @@
-﻿namespace ComponenteGenerico.DAL;
+﻿using ComponenteGenerico.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class Contexto
+namespace ComponenteGenerico.DAL;
+
+public class Contexto : DbContext
 {
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
 
+    public DbSet<Metas> Metas { get; set; }
+    public DbSet<Aportes> Aportes { get; set; }
+    public DbSet<Prioridades> Prioridades { get; set; }
 }
